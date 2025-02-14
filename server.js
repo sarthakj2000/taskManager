137d2a6d11fcd94d753d4d64e72aa024ee56fb27
Sarthak Jaiswal <sarthakjaiswal45@gmail.com>
2025-02-14 21:25:30 +0530

    third

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-
 const app = express();
-
-// Connect Database
+//connect database
 connectDB();
+const PORT = process.env.PORT || 5000;
+//Init Middleware
+app.use(express.json({ extented: true })); //for req.body
 
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-// Define Routes
+// app.get('/',(req,res)=>res.json({msg:'welcome to the contact keeper api'}));
+//Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  // The catch-all handler: for any request that is not an API request,
+  // send back the React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
